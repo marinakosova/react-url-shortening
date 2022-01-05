23f2cfe55fae5da42a9d0774070390a0d25452d9
marinakosova <marina.kosova@gmail.com>
2022-01-05 15:41:57 -0800

    Add store, animation, persistor

--- src/components/Shortens/Shortens.jsx
+++ src/components/Shortens/Shortens.jsx
@@ -1,9 +1,20 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { motion, AnimatePresence } from 'framer-motion';
+import { selectLinks } from 'store/slice/linkSlice';
 import {Button} from 'components/Button';
 import classes from './Shortens.module.scss';
 
 
 const Shortens = () => {
-    const links = [];
+    const [copiedLinks, setCopiedLink] = useState(null);
+    const links = useSelector(selectLinks);
+
+    const copyToClipboard = (link) => {
+        navigator.clipboard.writeText(link).then(() => {
+            setCopiedLink(link);
+        })
+    };
 
     if (!links?.length) return null;
 
@@ -11,18 +22,23 @@ const Shortens = () => {
         <section className={classes.Shortens}>
             <div className='container'>
                 {links.map(item => (
-                    <div
-                        key={item.code}
-                        className={classes.item}
+                    <AnimatePresence key={item.code}>
+                    <motion.div
+                        className={classes.item} 
+                        data-active={copiedLinks === item.full_short_link2} 
+                        initial={{opacity: 0, height: 0}} 
+                        animate={{opacity: 1, height: 'auto'}}
                     >
                         <span>{item.original_link}</span>
                         <span>{item.full_short_link2}</span>
                         <Button
-                            variant="square"
+                            variant="square" 
+                            onClick={() => copyToClipboard(item.full_short_link2)}
                         >
-                            Copy
+                            {copiedLinks === item.full_short_link2 ? 'Copied!' : 'Copy'} 
                         </Button>
-                    </div>
+                    </motion.div>
+                    </AnimatePresence>
                 ))}
             </div>
         </section>
